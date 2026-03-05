c10631daac673dd9dd2f81bc0f2a280644726368
Daryn Gregory <daryn.greg@icloud.com>
2026-03-04 23:31:04 -0500

    created employees database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
-DROP TABLE IF EXISTS employees;
+
+-- In development and testing environments, it is useful to drop and recreate
+-- tables to ensure a "fresh" start. This is not something we want to be doing in
+-- production!DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    birthday DATE NOT NULL,
+    salary INTEGER NOT NULL
+)
 
 
